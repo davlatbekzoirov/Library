4a7806309fb7daf3a5a1f4390306ecfeac85a763
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-08-22 22:19:47 +0500

    created Book models

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title','author','isbn','price')
+    search_fields = ('title','author','isbn','price')
+    list_filter = ('author','isbn','price')
+    list_per_page = 10
+
+admin.site.register(Book,BookAdmin)
