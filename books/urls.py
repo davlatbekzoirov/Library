a6c1521a9daa36ea23499b15c40a6d503d431e3d
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-08-22 22:37:25 +0500

    using def

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.BookListAPIView.as_view(), name='index'),
+    # path('', views.BookListAPIView.as_view()),
+    path('', views.book_list_view),
 ]
