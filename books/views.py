304c4fc1d05e6d0e4c2cfce02cfc40f60622a3aa
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-08-25 19:42:08 +0500

    to APIView

--- books/views.py
+++ books/views.py
@@ -1,13 +1,26 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
 
-class BookListAPIView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+# class BookListAPIView(generics.ListAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+class BookListAPIView(APIView):
+
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True).data
+        data = {
+            'status': f'Returned {len(books)} books',
+            'books': serializer
+        }
+
+        return Response(data)
 
 class BookDetailAPIView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
