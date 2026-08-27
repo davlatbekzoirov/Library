03f0fc2761d31315e19d6a9d2835a76fd1aaa845
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-08-27 22:32:55 +0500

    added validators

--- books/serializers.py
+++ books/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('id', 'title', 'subtitle', 'content', 'author', 'isbn', 'price',)
+
+    def validate(self, data):
+        title = data.get('title', None)
+        author = data.get('author', None)
+
+        if not title.isalpha():
+            raise ValidationError({
+                'status': False,
+                'message': 'Title must only contain letters, numbers and underscores.',
+            })
+
+        if Book.objects.filter(title=title, author=author).exists():
+            raise ValidationError({
+                'status': False,
+                'message': 'Title already exists.',
+            })
+
+        return data
